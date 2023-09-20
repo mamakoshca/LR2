688745ea0d68f9c5706e2865d20a246bb377d2bd
mamakoshca <enotartur5@gmail.com>
2023-09-20 13:20:19 +0700

    LR2
    LR2

---
+++ LB2.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "RU");
+	int a, b, c;
+	cout << "Введите первый катет: " << endl;
+	cin >> a;
+	cout << "Введите второй катет: " << endl;
+	cin >> b;
+	c = sqrt(a * a + b * b);
+	cout << "Гипотенуза = " << c << endl;
+	cout << "Периметр = " << a + b + c << endl;
+	return 0;
+}
---
+++ LB2.2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Введите число: " << endl;
+	cin >> a;
+	cout << "Результат: " << (a / 100) * 100 << endl;
+	return 0;
+}
